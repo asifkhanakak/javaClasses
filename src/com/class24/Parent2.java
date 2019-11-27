21715f14755df3a8039f253ee8053b6dc1fb8059
unknown <emaleasif@gmail.com>
2019-11-26 22:44:13 -0500

    class 26 commited

---
+++ src/com/class24/Parent2.java
@@ -0,0 +1,7 @@
+package com.class24;
+
+public class Parent2 {
+	
+	public static String race="Hispanic";
+
+}
---
+++ src/com/class26/ChromeDriver.java
@@ -0,0 +1,14 @@
+package com.class26;
+
+
+	
+	public class ChromeDriver extends WebDriver{
+
+		public void open() {
+			System.out.println("Opening chrome browser");
+		}
+	
+	
+	
+
+}
